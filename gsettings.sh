98ef179ee3499799cd7759c83b638f6832abc63e
William Olofsson Maronen <williamolofssonmaronen@gmail.com>
2025-06-17 21:49:58 +0200

    Add dock settings

--- gsettings.sh
+++ gsettings.sh
@@ -17,3 +17,7 @@ for i in {1..9}; do
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Shift><Alt>$i']"
 done
 gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-last "['<Shift><Alt>0']"
+
+# Set dock position to the bottom
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position 'BOTTOM'
+
