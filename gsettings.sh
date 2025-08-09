b1820fdbaba88d23f26aa6d791574e6d2c4d7798
William Olofsson Maronen <williamolofssonmaronen@gmail.com>
2025-08-09 22:07:08 +0200

    Add print statements

--- gsettings.sh
+++ gsettings.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # Disable dynamic workspaces
+echo 'Disabling dynamic workspaces...'
 gsettings set org.gnome.mutter dynamic-workspaces false
 
 # Set 10 fixed workspaces
+echo 'Set 10 fixed workspaces...'
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
 
 # Switch to workspace
+echo 'Set switch to workspace keybinds...'
 for i in {1..9}; do
   gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-$i "['<Alt>$i']"
 done
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-last "['<Alt>0']"
 
 # Move to workspace
+echo 'Set move to workspace keybinds...'
 for i in {1..9}; do
   gsettings set org.gnome.desktop.wm.keybindings move-to-workspace-$i "['<Shift><Alt>$i']"
 done
